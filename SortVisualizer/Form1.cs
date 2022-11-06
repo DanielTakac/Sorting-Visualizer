d39d79c69b41e69aa823409583fc4c9008534b1e
DanielTakac <danieltakac@protonmail.com>
2022-11-06 22:06:01 +0100

    Inital

---
+++ SortVisualizer/Form1.cs
@@ -0,0 +1,11 @@
+namespace SortVisualizer {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
