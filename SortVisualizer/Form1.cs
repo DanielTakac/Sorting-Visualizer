48ea1d6b2ae2f6b0644b96a5259367bb3d2d5f4b
DanielTakac <danieltakac@protonmail.com>
2022-11-06 22:36:50 +0100

    .

--- SortVisualizer/Form1.cs
+++ SortVisualizer/Form1.cs
@@ -13,7 +13,28 @@ namespace SortVisualizer {
 
         private void btnReset_Click(object sender, EventArgs e) {
 
-            
+            g = panel1.CreateGraphics();
+
+            int numEntries = panel1.Width;
+            int maxValue = panel1.Height;
+
+            theArray = new int[numEntries];
+
+            g.FillRectangle(new SolidBrush(Color.Black), 0, 0, numEntries, maxValue);
+
+            Random rand = new Random();
+
+            for (int i = 0; i < numEntries; i++) {
+
+                theArray[i] = rand.Next(0, maxValue);
+
+            }
+
+            for (int i = 0; i < numEntries; i++) {
+
+                g.FillRectangle(new SolidBrush(Color.White), i, maxValue - theArray[i], 1, maxValue);
+
+            }
 
         }
 
